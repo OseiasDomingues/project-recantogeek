9ec88a742ee3416223774126e7bba1b63793c121
OseiasDomingues <oseiasdomingues9@gmail.com>
2021-10-31 17:54:03 -0300

    initial commit

---
+++ src/main/java/com/projectdl/recantogeek/RecantoGeekApplication.java
@@ -0,0 +1,13 @@
+package com.projectdl.recantogeek;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecantoGeekApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecantoGeekApplication.class, args);
+	}
+
+}
