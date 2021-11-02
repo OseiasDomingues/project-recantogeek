78ad8f5a91d06b73eb87040c4a1b15eb25e29763
OseiasDomingues <oseiasdomingues9@gmail.com>
2021-11-02 15:07:35 -0300

    css viewProductList.html

--- src/main/java/com/projectdl/recantogeek/utils/DummyData.java
+++ src/main/java/com/projectdl/recantogeek/utils/DummyData.java
@@ -22,7 +22,15 @@ public class DummyData {
         ProductModel p2 = new ProductModel(null, "Moleton 02", BigDecimal.valueOf(20.50));
         ProductModel p3 = new ProductModel(null, "Moleton 03", BigDecimal.valueOf(10.50));
         ProductModel p4 = new ProductModel(null, "Moleton 04", BigDecimal.valueOf(12.50));
+        ProductModel p5 = new ProductModel(null, "Moleton 05", BigDecimal.valueOf(17.50));
+        ProductModel p6 = new ProductModel(null, "Moleton 06", BigDecimal.valueOf(20.50));
+        ProductModel p7 = new ProductModel(null, "Moleton 07", BigDecimal.valueOf(10.50));
+        ProductModel p8 = new ProductModel(null, "Moleton 08", BigDecimal.valueOf(12.50));
+        ProductModel p9 = new ProductModel(null, "Moleton 09", BigDecimal.valueOf(17.50));
+        ProductModel p10 = new ProductModel(null, "Moleton 10", BigDecimal.valueOf(20.50));
+        ProductModel p11 = new ProductModel(null, "Moleton 11", BigDecimal.valueOf(10.50));
+        ProductModel p12 = new ProductModel(null, "Moleton 12", BigDecimal.valueOf(12.50));
 
-        productRepository.saveAll(Arrays.asList(p1,p2,p3,p4));
+        productRepository.saveAll(Arrays.asList(p1,p2,p3,p4,p5,p6,p7,p8,p9,p10,p11,p12));
     }
 }
